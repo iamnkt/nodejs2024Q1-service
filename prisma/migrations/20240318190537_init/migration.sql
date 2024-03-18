545a1fc55a3a5dc91d44884eae4e3067cac5184c
iamnkt <sg23mj@gmail.com>
2024-03-19 01:13:27 +0300

    feat: add prisma schema, do migration to postgresql, add database service

---
+++ prisma/migrations/20240318190537_init/migration.sql
@@ -0,0 +1,51 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "version" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Artist" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "grammy" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Artist_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Album" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "artistId" TEXT,
+
+    CONSTRAINT "Album_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Track" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "artistId" TEXT,
+    "albumId" TEXT,
+    "duration" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "Track_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Favs" (
+    "id" TEXT NOT NULL,
+    "artists" TEXT[],
+    "albums" TEXT[],
+    "tracks" TEXT[],
+
+    CONSTRAINT "Favs_pkey" PRIMARY KEY ("id")
+);
