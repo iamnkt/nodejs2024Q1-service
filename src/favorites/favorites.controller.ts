2feee41c4f52ff3b7368619a64d7b938abbafbfc
iamnkt <sg23mj@gmail.com>
2024-03-20 16:11:58 +0300

    fix: fix favs service and controller to work with postgresql database

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -8,29 +8,28 @@ import {
   ParseUUIDPipe,
   Post,
 } from '@nestjs/common';
-import { FavoritesResponse } from './entity';
 import { FavoritesService } from './favorites.service';
 
 @Controller('favs')
 export class FavoritesController {
   constructor(private favoritesService: FavoritesService) {}
 
-  // @Get()
-  // getAll(): FavoritesResponse {
-  //   return this.favoritesService.findAll();
-  // }
+  @Get()
+  getAll() {
+    return this.favoritesService.findAll();
+  }
 
-  // @Post(':route/:id')
-  // add(@Param('route') route: string, @Param('id', ParseUUIDPipe) id: string) {
-  //   return this.favoritesService.add(route, id);
-  // }
+  @Post(':route/:id')
+  add(@Param('route') route: string, @Param('id', ParseUUIDPipe) id: string) {
+    return this.favoritesService.add(route, id);
+  }
 
-  // @Delete(':route/:id')
-  // @HttpCode(HttpStatus.NO_CONTENT)
-  // delete(
-  //   @Param('route') route: string,
-  //   @Param('id', ParseUUIDPipe) id: string,
-  // ) {
-  //   this.favoritesService.remove(route, id);
-  // }
+  @Delete(':route/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(
+    @Param('route') route: string,
+    @Param('id', ParseUUIDPipe) id: string,
+  ) {
+    return this.favoritesService.remove(route, id);
+  }
 }
--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,35 +1,138 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { FavoritesResponse } from './entity';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class FavoritesService {
-  // findAll(): FavoritesResponse {
-  //   const favorites = db.getFavs();
-  //   return favorites;
-  // }
-  // add(route: string, id: string) {
-  //   const record = db.findRecord(route, id);
-  //   if (!record) {
-  //     throw new HttpException(
-  //       `${route} not found`,
-  //       HttpStatus.UNPROCESSABLE_ENTITY,
-  //     );
-  //   }
-  //   const recordId = record.id;
-  //   const favorite = db.findFavorite(route, id);
-  //   if (!favorite) {
-  //     db.addToFavorites(route, recordId);
-  //   }
-  //   return record;
-  // }
-  // remove(route: string, id: string) {
-  //   const record = db.findRecord(route, id);
-  //   const favorite = db.findFavorite(route, id);
-  //   if (record && !favorite) {
-  //     throw new HttpException(`${route} is not favorite`, HttpStatus.NOT_FOUND);
-  //   }
-  //   if (favorite) {
-  //     db.removeFromFavorites(route, id);
-  //   }
-  // }
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async findAll() {
+    let favs = (await this.databaseService.favs.findMany({}))[0];
+
+    if (!favs) {
+      const favsData = {
+        id: crypto.randomUUID(),
+        artists: [],
+        albums: [],
+        tracks: [],
+      };
+
+      this.databaseService.favs.create({ data: favsData });
+      favs = (await this.databaseService.favs.findMany({}))[0];
+    }
+
+    const artists = await Promise.all(
+      favs.artists.map(async (id) => {
+        return await this.databaseService.artist.findUnique({
+          where: {
+            id,
+          },
+        });
+      }),
+    );
+
+    const albums = await Promise.all(
+      favs.albums.map(async (id) => {
+        return await this.databaseService.album.findUnique({
+          where: {
+            id,
+          },
+        });
+      }),
+    );
+
+    const tracks = await Promise.all(
+      favs.tracks.map(async (id) => {
+        return await this.databaseService.track.findUnique({
+          where: {
+            id,
+          },
+        });
+      }),
+    );
+
+    const result = {
+      artists: artists.filter((artist) => artist !== null),
+      albums: albums.filter((album) => album !== null),
+      tracks: tracks.filter((track) => track !== null),
+    };
+
+    return result;
+  }
+
+  async add(route: string, id: string) {
+    const record = await this.databaseService[route].findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!record) {
+      throw new HttpException(
+        `${route} not found`,
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    let favs = (await this.databaseService.favs.findMany({}))[0];
+
+    if (!favs) {
+      const favsData = {
+        id: crypto.randomUUID(),
+        artists: [],
+        albums: [],
+        tracks: [],
+      };
+
+      await this.databaseService.favs.create({ data: favsData });
+      favs = (await this.databaseService.favs.findMany({}))[0];
+    }
+
+    const favorite = favs[`${route}s`].find((favoriteId) => favoriteId === id);
+
+    if (!favorite) {
+      favs[`${route}s`].push(id);
+
+      await this.databaseService.favs.update({
+        where: {
+          id: favs.id,
+        },
+        data: {
+          [`${route}s`]: favs[`${route}s`],
+        },
+      });
+    }
+
+    return record;
+  }
+
+  async remove(route: string, id: string) {
+    const record = this.databaseService[route].findUnique({
+      where: {
+        id,
+      },
+    });
+
+    const favs = (await this.databaseService.favs.findMany({}))[0];
+
+    const favorite = favs[`${route}s`].find((favoriteId) => favoriteId === id);
+
+    if (record && !favorite) {
+      throw new HttpException(`${route} is not favorite`, HttpStatus.NOT_FOUND);
+    }
+
+    if (favorite) {
+      const idxRemoveTo = favs[`${route}s`].indexOf(id);
+
+      favs[`${route}s`].splice(idxRemoveTo, 1);
+
+      return await this.databaseService.favs.update({
+        where: {
+          id: favs.id,
+        },
+        data: {
+          [`${route}s`]: favs[`${route}s`],
+        },
+      });
+    }
+  }
 }
